3f5db5e38e89f85640c880d1301eb77862c5169d
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-11-18 08:08:47 -0600

    added side-menu @include( )

---
+++ resources/views/includes/side-menu.blade.php
@@ -0,0 +1,52 @@
+ <div class="welcome-menu">
+  <div class="close">
+    <i class="fa fa-times" aria-hidden="true" alt="Close"></i>
+  </div>
+  <div class="logo">
+    <a href="/"><img src=".././public/img/mama-mia-logo-pizza-angle.png" alt="Mamma Mia logo" /></a>
+  </div>
+  <div class="menu">
+    <ul>
+      <a href="menu"><li>Menu</li></a>
+      <a href="offers"><li>Rewards</li></a>
+      <a href="waitlist"><li>Wait List</li></a>
+      <a href="giftcards"><li>Gift Cards</li></a>
+    </ul>
+    <ul class="social-icons">
+      <li>
+        <a href="www.facebook.com">
+
+          <img
+            src=".././public/img/icons/facebook.png"
+            alt="Facebook"
+            title="Facebook"
+          />
+        </a>
+      </li>
+      <li>
+        <a href="www.twitter.com">
+          <img
+            src=".././public/img/icons/twitter.png"
+            alt="Twitter"
+            title="Twitter"
+          />
+        </a>
+      </li>
+      <li>
+        <a href="www.instagram.com">
+          <img
+            src=".././public/img/icons/instagram.png"
+            alt="Instagram"
+            title="Instagram"
+          />
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="location">
+    <div class="address">1234 Main St.<br />Chicago, IL</div>
+    <div class="phone-number">
+      <a href="tel:+7085551234">(708)555-1234</a>
+    </div>
+  </div>
+  </div>
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-// Route::get('/home', function () {
-//     return view('views.home');
-// });
 Route::get('/menu', function () {
     return view('menu.index');
 });
